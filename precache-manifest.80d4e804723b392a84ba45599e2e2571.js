2fc6c4b3627dbd8db5c99d5dab86b9400469017a
krizoeris <krizoeris@yahoo.com.ph>
2020-03-22 19:40:47 +0400

    Updates

---
+++ precache-manifest.80d4e804723b392a84ba45599e2e2571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272da1a95f94c9c5a133d53410036e11",
+    "url": "/Cryptowatch/index.html"
+  },
+  {
+    "revision": "694090e741cb7e69ca64",
+    "url": "/Cryptowatch/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0ae79c90cb18017058e6",
+    "url": "/Cryptowatch/static/js/2.a729241f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cryptowatch/static/js/2.a729241f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694090e741cb7e69ca64",
+    "url": "/Cryptowatch/static/js/main.9b3f12ba.chunk.js"
+  },
+  {
+    "revision": "1ec1f13655738dbdff16",
+    "url": "/Cryptowatch/static/js/runtime-main.c9c5041f.js"
+  }
+]);
