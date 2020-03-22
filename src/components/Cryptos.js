057416bb5b9c1977ba8d536ef1c4c3eea0ffd53b
Kriztian Eris Labatete <kriztian.eris@gmail.com>
2020-03-22 18:25:59 +0400

    First Commit

---
+++ src/components/Cryptos.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Cryptos = () => {
+    return(
+        <div>
+        </div>
+    )
+}
+
+export default Cryptos
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        <div className="mt-8 mb-8">
+            <h2 className="font-semibold text-gray-800 text-5xl mb-2">CryptoWatch</h2>
+            <h4 className="font-semibold text-gray-600 text-xl">Displays the top coins by marketcap and the top gainers & losers</h4>
+        </div>
+    )
+}
+
+export default Header
