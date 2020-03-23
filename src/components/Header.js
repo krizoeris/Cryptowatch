a00c3b8e43e70407136fdd4e86ba41ad66f8825d
krizoeris <krizoeris@yahoo.com.ph>
2020-03-24 00:35:12 +0400

    Rephrase desc

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return(
         <div className="pt-8 pb-8 mb-8 text-center bg-gray-200">
             <h2 className="font-bold text-gray-800 text-5xl mb-2">CryptoWatch</h2>
-            <h4 className="font-semibold text-gray-600 text-xl">Displays the top coins by marketcap and the top gainers & losers</h4>
+            <h4 className="font-semibold text-gray-600 text-xl">📈 Displays the coins by market cap and the latest price change</h4>
         </div>
     )
 }
